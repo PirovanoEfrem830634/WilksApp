5d81b2f6e096e0a9a0dd918279c8f3ea172f8af9
PirovanoEfrem830634 <e.pirovano8@campus.unimib.it>
2025-04-07 16:24:52 +0200

    modifiche varie

--- firebaseconfig.ts
+++ firebaseconfig.ts
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Configurazione Firebase (usa i dati copiati da Firebase)
+// Configurazione Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyDkLKo397p5BeuC6x7cauFbvjuO9QbeMDY",
   authDomain: "wilksapp.firebaseapp.com",
@@ -12,9 +12,10 @@ const firebaseConfig = {
   appId: "1:892270456868:web:5b34f244260f7787ff68b1",
 };
 
-// Inizializzazione di Firebase
-const app = initializeApp(firebaseConfig);
+// Inizializzazione Firebase
+const firebaseApp = initializeApp(firebaseConfig);
 
-// Export dei servizi di Auth e Firestore
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+// Export dei servizi
+export const auth = getAuth(firebaseApp);
+export const db = getFirestore(firebaseApp);
+export const app = firebaseApp; // ✅ adesso l’export ha un nome diverso dalla dichiarazione
