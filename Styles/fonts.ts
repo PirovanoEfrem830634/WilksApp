cf5f02f4efd13ec404cc940e36029f97362b9034
PirovanoEfrem830634 <e.pirovano8@campus.unimib.it>
2026-01-07 17:26:48 +0100

    no more useless fonts

--- Styles/fonts.ts
+++ Styles/fonts.ts
@@ -2,54 +2,6 @@ import * as Font from "expo-font";
 
 export async function loadFonts() {
   await Font.loadAsync({
-    // Montserrat Fonts
-    "Montserrat-Black": require("../assets/fonts/Montserrat-Black.ttf"),
-    "Montserrat-BlackItalic": require("../assets/fonts/Montserrat-BlackItalic.ttf"),
-    "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.ttf"),
-    "Montserrat-BoldItalic": require("../assets/fonts/Montserrat-BoldItalic.ttf"),
-    "Montserrat-ExtraBold": require("../assets/fonts/Montserrat-ExtraBold.ttf"),
-    "Montserrat-ExtraBoldItalic": require("../assets/fonts/Montserrat-ExtraBoldItalic.ttf"),
-    "Montserrat-ExtraLight": require("../assets/fonts/Montserrat-ExtraLight.ttf"),
-    "Montserrat-ExtraLightItalic": require("../assets/fonts/Montserrat-ExtraLightItalic.ttf"),
-    "Montserrat-Italic": require("../assets/fonts/Montserrat-Italic.ttf"),
-    "Montserrat-Light": require("../assets/fonts/Montserrat-Light.ttf"),
-    "Montserrat-LightItalic": require("../assets/fonts/Montserrat-LightItalic.ttf"),
-    "Montserrat-Medium": require("../assets/fonts/Montserrat-Medium.ttf"),
-    "Montserrat-MediumItalic": require("../assets/fonts/Montserrat-MediumItalic.ttf"),
-    "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.ttf"),
-    "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.ttf"),
-    "Montserrat-SemiBoldItalic": require("../assets/fonts/Montserrat-SemiBoldItalic.ttf"),
-    "Montserrat-Thin": require("../assets/fonts/Montserrat-Thin.ttf"),
-    "Montserrat-ThinItalic": require("../assets/fonts/Montserrat-ThinItalic.ttf"),
-
-    // Open Sans Condensed Fonts
-    "OpenSans_Condensed-Bold": require("../assets/fonts/OpenSans_Condensed-Bold.ttf"),
-    "OpenSans_Condensed-BoldItalic": require("../assets/fonts/OpenSans_Condensed-BoldItalic.ttf"),
-    "OpenSans_Condensed-ExtraBold": require("../assets/fonts/OpenSans_Condensed-ExtraBold.ttf"),
-    "OpenSans_Condensed-ExtraBoldItalic": require("../assets/fonts/OpenSans_Condensed-ExtraBoldItalic.ttf"),
-    "OpenSans_Condensed-Italic": require("../assets/fonts/OpenSans_Condensed-Italic.ttf"),
-    "OpenSans_Condensed-Light": require("../assets/fonts/OpenSans_Condensed-Light.ttf"),
-    "OpenSans_Condensed-LightItalic": require("../assets/fonts/OpenSans_Condensed-LightItalic.ttf"),
-    "OpenSans_Condensed-Medium": require("../assets/fonts/OpenSans_Condensed-Medium.ttf"),
-    "OpenSans_Condensed-MediumItalic": require("../assets/fonts/OpenSans_Condensed-MediumItalic.ttf"),
-    "OpenSans_Condensed-Regular": require("../assets/fonts/OpenSans_Condensed-Regular.ttf"),
-    "OpenSans_Condensed-SemiBold": require("../assets/fonts/OpenSans_Condensed-SemiBold.ttf"),
-    "OpenSans_Condensed-SemiBoldItalic": require("../assets/fonts/OpenSans_Condensed-SemiBoldItalic.ttf"),
-
-    // Open Sans Fonts
-    "OpenSans-Bold": require("../assets/fonts/OpenSans-Bold.ttf"),
-    "OpenSans-BoldItalic": require("../assets/fonts/OpenSans-BoldItalic.ttf"),
-    "OpenSans-ExtraBold": require("../assets/fonts/OpenSans-ExtraBold.ttf"),
-    "OpenSans-ExtraBoldItalic": require("../assets/fonts/OpenSans-ExtraBoldItalic.ttf"),
-    "OpenSans-Italic": require("../assets/fonts/OpenSans-Italic.ttf"),
-    "OpenSans-Light": require("../assets/fonts/OpenSans-Light.ttf"),
-    "OpenSans-LightItalic": require("../assets/fonts/OpenSans-LightItalic.ttf"),
-    "OpenSans-Medium": require("../assets/fonts/OpenSans-Medium.ttf"),
-    "OpenSans-MediumItalic": require("../assets/fonts/OpenSans-MediumItalic.ttf"),
-    "OpenSans-Regular": require("../assets/fonts/OpenSans-Regular.ttf"),
-    "OpenSans-SemiBold": require("../assets/fonts/OpenSans-SemiBold.ttf"),
-    "OpenSans-SemiBoldItalic": require("../assets/fonts/OpenSans-SemiBoldItalic.ttf"),
-
     // San Francisco Fonts
     "SFProDisplay-Black": require("../assets/fonts/SF-Pro-Display-Black.otf"),
     "SFProDisplay-BlackItalic": require("../assets/fonts/SF-Pro-Display-BlackItalic.otf"),
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,60 +5,12 @@ import { useEffect } from "react";
 
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({
-
-    // San Francisco Fonts
-    "SFProDisplay-Black": require("../assets/fonts/SF-Pro-Display-Black.otf"),
-    "SFProDisplay-BlackItalic": require("../assets/fonts/SF-Pro-Display-BlackItalic.otf"),
-    "SFProDisplay-Bold": require("../assets/fonts/SF-Pro-Display-Bold.otf"),
-    "SFProDisplay-BoldItalic": require("../assets/fonts/SF-Pro-Display-BoldItalic.otf"),
-    "SFProDisplay-Heavy": require("../assets/fonts/SF-Pro-Display-Heavy.otf"),
-    "SFProDisplay-HeavyItalic": require("../assets/fonts/SF-Pro-Display-HeavyItalic.otf"),
-    "SFProDisplay-Light": require("../assets/fonts/SF-Pro-Display-Light.otf"),
-    "SFProDisplay-LightItalic": require("../assets/fonts/SF-Pro-Display-LightItalic.otf"),
-    "SFProDisplay-Medium": require("../assets/fonts/SF-Pro-Display-Medium.otf"),
-    "SFProDisplay-MediumItalic": require("../assets/fonts/SF-Pro-Display-MediumItalic.otf"),
+    // ✅ Keep only what you actually use
     "SFProDisplay-Regular": require("../assets/fonts/SF-Pro-Display-Regular.otf"),
-    "SFProDisplay-RegularItalic": require("../assets/fonts/SF-Pro-Display-RegularItalic.otf"),
     "SFProDisplay-Semibold": require("../assets/fonts/SF-Pro-Display-Semibold.otf"),
-    "SFProDisplay-SemiboldItalic": require("../assets/fonts/SF-Pro-Display-SemiboldItalic.otf"),
-    "SFProDisplay-Thin": require("../assets/fonts/SF-Pro-Display-Thin.otf"),
-    "SFProDisplay-ThinItalic": require("../assets/fonts/SF-Pro-Display-ThinItalic.otf"),
-    "SFProDisplay-Ultralight": require("../assets/fonts/SF-Pro-Display-Ultralight.otf"),
-    "SFProDisplay-UltralightItalic": require("../assets/fonts/SF-Pro-Display-UltralightItalic.otf"),
-
-    // Montserrat Fonts
-    "Montserrat-Black": require("../assets/fonts/Montserrat-Black.ttf"),
-    "Montserrat-BlackItalic": require("../assets/fonts/Montserrat-BlackItalic.ttf"),
-    "Montserrat-Bold": require("../assets/fonts/Montserrat-Bold.ttf"),
-    "Montserrat-BoldItalic": require("../assets/fonts/Montserrat-BoldItalic.ttf"),
-    "Montserrat-ExtraBold": require("../assets/fonts/Montserrat-ExtraBold.ttf"),
-    "Montserrat-ExtraBoldItalic": require("../assets/fonts/Montserrat-ExtraBoldItalic.ttf"),
-    "Montserrat-ExtraLight": require("../assets/fonts/Montserrat-ExtraLight.ttf"),
-    "Montserrat-ExtraLightItalic": require("../assets/fonts/Montserrat-ExtraLightItalic.ttf"),
-    "Montserrat-Italic": require("../assets/fonts/Montserrat-Italic.ttf"),
-    "Montserrat-Light": require("../assets/fonts/Montserrat-Light.ttf"),
-    "Montserrat-LightItalic": require("../assets/fonts/Montserrat-LightItalic.ttf"),
-    "Montserrat-Medium": require("../assets/fonts/Montserrat-Medium.ttf"),
-    "Montserrat-MediumItalic": require("../assets/fonts/Montserrat-MediumItalic.ttf"),
-    "Montserrat-Regular": require("../assets/fonts/Montserrat-Regular.ttf"),
-    "Montserrat-SemiBold": require("../assets/fonts/Montserrat-SemiBold.ttf"),
-    "Montserrat-SemiBoldItalic": require("../assets/fonts/Montserrat-SemiBoldItalic.ttf"),
-    "Montserrat-Thin": require("../assets/fonts/Montserrat-Thin.ttf"),
-    "Montserrat-ThinItalic": require("../assets/fonts/Montserrat-ThinItalic.ttf"),
-
-    // OpenSans Fonts
-    "OpenSans-Bold": require("../assets/fonts/OpenSans-Bold.ttf"),
-    "OpenSans-BoldItalic": require("../assets/fonts/OpenSans-BoldItalic.ttf"),
-    "OpenSans-ExtraBold": require("../assets/fonts/OpenSans-ExtraBold.ttf"),
-    "OpenSans-ExtraBoldItalic": require("../assets/fonts/OpenSans-ExtraBoldItalic.ttf"),
-    "OpenSans-Italic": require("../assets/fonts/OpenSans-Italic.ttf"),
-    "OpenSans-Light": require("../assets/fonts/OpenSans-Light.ttf"),
-    "OpenSans-LightItalic": require("../assets/fonts/OpenSans-LightItalic.ttf"),
-    "OpenSans-Medium": require("../assets/fonts/OpenSans-Medium.ttf"),
-    "OpenSans-MediumItalic": require("../assets/fonts/OpenSans-MediumItalic.ttf"),
-    "OpenSans-Regular": require("../assets/fonts/OpenSans-Regular.ttf"),
-    "OpenSans-SemiBold": require("../assets/fonts/OpenSans-SemiBold.ttf"),
-    "OpenSans-SemiBoldItalic": require("../assets/fonts/OpenSans-SemiBoldItalic.ttf"),
+    "SFProDisplay-Bold": require("../assets/fonts/SF-Pro-Display-Bold.otf"),
+    "SFProDisplay-Black": require("../assets/fonts/SF-Pro-Display-Black.otf"),
+    "SFProDisplay-Medium": require("../assets/fonts/SF-Pro-Display-Medium.otf"),
   });
 
   useEffect(() => {
@@ -70,5 +22,4 @@ export default function RootLayout() {
   if (!fontsLoaded) return null;
 
   return <Stack screenOptions={{ headerShown: false }} />;
-  
 }
